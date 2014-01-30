102e26477742a42a08776d89e3d523f9ce93d361
cnavarre <cnavarre@gmail.com>
2014-01-30 13:48:54 +0100

    Create simu1.R
    Function to create a simulation scenario selecting n neighbourhoods from expected observations of prostate cancer in Valencia.

---
+++ simu1.R
@@ -0,0 +1,44 @@
+load("./data//input_data.Rdata")
+
+# Definición del vector de thetas
+v.theta = c(1.5,2,3)
+
+# Factor de escala para la los valores esperados
+v.SF = 2:10
+perc.rank <- function(x) trunc(rank(x))/length(x)
+
+simu1 <- function( v.exp,n,theta,SF=1 ) {
+  # Factor de escala para los valores esperados
+  v.exp <- v.exp * SF
+  
+  # Calculamos los ranking por percentil de cada valor del vector:
+  df.prost <- data.frame( exp=v.exp )
+  df.prost <- within(df.prost, prank <- perc.rank(v.exp))
+  df.prost$id <- names(v.exp)
+
+  # Separamos la muestra en 'n' trozos y seleccionamos una muestra de cada trozo
+  breaks <- (0:n)/n
+  sel.idx <- vector()
+  for( i in 2:length(breaks) ) {
+    sel.idx <- c(sel.idx,
+                 sample( df.prost[df.prost$prank <= breaks[i] & 
+                    df.prost$prank > breaks[i-1], "id" ], size=1 ) )
+  }
+
+  # Asignamos mismo valor que el esperado para los no seleccionados
+  df.prost$obs[ !(df.prost$id %in% sel.idx) ] <- 
+    df.prost$exp[ !(df.prost$id %in% sel.idx) ]
+  # Asignamos el valor multiplicado por theta para cada punto seleccionado
+  df.prost$obs[ df.prost$id %in% sel.idx ] <- 
+    df.prost$exp[ df.prost$id %in% sel.idx ] * theta
+  
+  v.obs <- df.prost$obs
+  names(v.obs) <- df.prost$id
+  
+  # Devolvemos una lista con el valor esperado,valor observado y el identificador
+  return(list(exp=v.exp,obs=v.obs,sel=sel.idx))
+}
+
+simu1(Eprostata,5,v.theta[1])
+
+simu1(Eprostata,5,v.theta[2],SF=2)
